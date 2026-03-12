70b39461412a4047f3109c46339ea26e232076b2
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2026-03-12 16:11:01 +0400

    Добавлены классы и зависимости для документирования с помощью Swagger

--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,9 @@ import com.example.model.dto.UserRequest;
 import com.example.model.dto.UserResponse;
 import com.example.model.dto.UserUpdateRequest;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,24 +16,35 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Users", description = "API для управления пользователями")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Создание нового пользователя")
+    @ApiResponse(responseCode = "201", description = "Пользователь создан")
     public UserResponse createUser(@Valid @RequestBody UserRequest userRequest) {
         log.info("Create user request: {}", userRequest);
         return userService.createUser(userRequest);
     }
 
     @PutMapping
+    @Operation(summary = "Обновление пользователя")
+    @ApiResponse(responseCode = "200", description = "Пользователь обновлен")
     public UserResponse updateUser(@Valid @RequestBody UserUpdateRequest userRequest) {
         log.info("Update user request: {}", userRequest);
         return userService.updateUser(userRequest);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Получение пользователя по ID",
+            description = "Возвращает информацию о пользователе по его уникальному идентификатору"
+    )
+    @ApiResponse(responseCode = "200", description = "Пользователь найден")
+    @ApiResponse(responseCode = "404", description = "Пользователь не найден")
     public UserResponse getUser(@PathVariable Long id) {
         log.info("Get user request: {}", id);
         return userService.getUserById(id);
@@ -38,6 +52,8 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Удаление пользователя")
+    @ApiResponse(responseCode = "204", description = "Пользователь удален")
     public void deleteUser(@PathVariable Long id) {
         log.info("Delete user request: {}", id);
         userService.deleteUser(id);
--- user-service/src/main/java/com/example/model/dto/UserRequest.java
+++ user-service/src/main/java/com/example/model/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -12,13 +13,16 @@ import lombok.*;
 @AllArgsConstructor
 public class UserRequest {
 
+    @Schema(description = "Имя пользователя")
     @NotBlank(message = "Имя пользователя не может быть пустым")
     private String userName;
 
+    @Schema(description = "Email пользователя")
     @NotBlank(message = "Email не может быть пустым")
     @Email(message = "Email пользователя должен соответствовать шаблону name@domain.com")
     private String email;
 
+    @Schema(description = "Возраст пользователя")
     @Positive(message = "Возраст пользователя не может быть отрицательным числом")
     private Integer age;
 
